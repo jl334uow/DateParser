aa48d494d5ec3b71302358b7fc7878b53336a0d5
Justin Lս <lljustinlu@gmail.com>
2021-08-10 14:11:26 +1000

    Added month and year components

--- DateParser.cpp
+++ DateParser.cpp
@@ -37,5 +37,45 @@ int main () {
 	
 	// TODO: Read dates from input, parse the dates to find the one
 	//       in the correct format, and output in m/d/yyyy format
+    string date, month, day, year;
+    date = "0";
+    while(date != "-1"){
+        getline(cin, date);
+
+        size_t pos = date.find(' ');
+        month = date.substr(0, pos);
+        month = to_string(DateParser(month));
+        
+        //if not a valid month then skip the rest of this iteration
+        if(month == "0") continue;
+
+        size_t pos2 = date.find(",");
+        year = date.substr(pos2+2);
+        cout << year << endl;
+
+    }
+
+    if(month != "0"){
+            cout << month << "/" << day << "/" << year << endl;
+        }
 
 }
+
+
+/*
+Complete main() to read dates from input, one date per line.
+
+Each date's format must be as follows: March 1, 1990. Any date not following that format is incorrect and should be ignored.
+
+Use the substr() function to parse the string and extract the date. The input ends with -1 on a line alone. Output each correct date as: 3/1/1990.
+
+March 1, 1990
+April 2 1995
+7/15/20
+December 13, 2003
+-1
+
+3/1/1990
+12/13/2003
+
+*/
